107d74a015bb68a84eef90de829d73dc962bb359
krdzex <58869690+krdzex@users.noreply.github.com>
2021-12-30 17:04:27 +0100

    Delete .history directory

--- .history/mern-skeleton/src/config/config_20211230011529.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config()
-const config = {
-    port: process.env.PORT || 4400,
-    jwtSecret: process.env.JWT_SECRET ||  "YOUR_secret_key",
-    mongoUri: process.env.MONGO || "mongodb://localhost:27017/db-name"
-}
-export default config;
